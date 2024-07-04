7c18e2132a1e046c1a9b7ac478f516d66a201fb2
Ayoub Chegraoui <ayoubch807@gmail.com>
2024-07-04 21:25:42 +0100

    feat: add appropriate message warning for comparaison results

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,56 @@ import { isEqual, sortObject } from './utils';
 import { JsonObj } from './types';
 import JsonContainer from './components/json-container';
 
+
+enum Equal {
+    FALSE = 0,
+    TRUE = 1,
+    NEUTRAL = 2 // 2 is neutral value, means no results yet
+}
+
+
 function App() {
     const [json1, setJson1] = useState<JsonObj>();
     const [json2, setJson2] = useState<JsonObj>();
+    const [results, setResults] = useState<Number>(Equal.NEUTRAL);
 
     const compareObjects = (): void => {
         const obj1 = sortObject(json1);
         const obj2 = sortObject(json2);
         console.log(obj1);
         console.log(obj2);
-        console.log(isEqual(obj1, obj2));
+        const result = isEqual(obj1, obj2);
+        console.log(result);
+        setResults(result ? Equal.TRUE : Equal.FALSE);
     }
 
     const updateObjects = (value: JsonObj, key: Number): void => {
         if (key === 1) setJson1(value);
         if (key === 2) setJson2(value);
+        
+        // the changes of json objects will reset the results to neutral value
+        setResults(Equal.NEUTRAL);
     }
 
+    const getResultsClass = () => {
+        return results === Equal.NEUTRAL ? 'hide-result' : (results === Equal.TRUE ? 'green' : 'red');
+    }
+
+    const getResultsText = () => {
+        return results === Equal.TRUE ? 'Objects are equal!' : 'Objects are not equal!';
+    }
+
+
     return (
-        <div className="App">
-            <JsonContainer update={updateObjects} jsonKey={1} />
-            <button className="btn-compare"
-                onClick={compareObjects} disabled={!json1 || !json2}>Compare</button>
-            <JsonContainer update={updateObjects} jsonKey={2} />
-        </div>
+        <>
+            {results !== Equal.NEUTRAL && <div className={getResultsClass()}>{getResultsText()}</div>}
+            <div className="App">
+                <JsonContainer update={updateObjects} jsonKey={1} />
+                <button className="btn-compare"
+                    onClick={compareObjects} disabled={!json1 || !json2}>Compare</button>
+                <JsonContainer update={updateObjects} jsonKey={2} />
+            </div>
+        </>
     );
 }
 
